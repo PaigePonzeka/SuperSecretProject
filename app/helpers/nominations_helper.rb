cca234dfab6e01aa9269027886f3d66c1d5fb6f8
Paige Ponzeka <paigepon@gmail.com>
2011-09-23 16:30:18 -0400

    Support for nominations method to help make view less verbose, supporting add new place

--- app/helpers/nominations_helper.rb
+++ app/helpers/nominations_helper.rb
@@ -1,2 +1,16 @@
 module NominationsHelper
+
+# returns an array of previously nominated places
+def previousNominations
+  # TODO remove duplicates and return
+ nominations= Nomination.where(:user_id => current_user).order("place_id ASC")
+
+end
+def addPlace
+  params[:new_place]
+end
+def addPlaceName
+  place = Place.find(addPlace)
+  place.name
+end
 end
